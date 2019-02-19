a30e820ca683920e00992b0e8fe53b5cdd83fd52
Jason M. Edgerton <J.EDGERTON@labs.clark.edu>
2019-02-19 12:53:23 -0800

    Change 1

--- page.php
+++ page.php
@@ -11,16 +11,28 @@
 </head>
 <body>
 <div class="container">
-    <section>
-        <h1>Evergreen</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates at reiciendis expedita adipisci sint? Esse quas repellendus obcaecati dicta. Iusto voluptates dicta dolor omnis necessitatibus labore amet voluptatem distinctio repudiandae.</p>
+    <div class="row">
+    <div class="col-sm-6 col-md-8 col-lg-12">
+    <section class="bg-info text-white p-5">
+        <img src="img/200px-Clark_College_logo.png" class="d-block mx-auto" alt="">
+
+        <div class="alert alert-success text-center font-weight-bold p-4 mt-4">
+            IT'S SNOWING 
+        </div>
+        <div class="h1 mt-4 d-sm-none d-log-block">Evergreen</div>
+        <p class="fong-weight-bold">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates at reiciendis expedita adipisci sint? Esse quas repellendus obcaecati dicta. Iusto voluptates dicta dolor omnis necessitatibus labore amet voluptatem distinctio repudiandae.</p>
         <h2>Camas</h2>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius enim, minus cumque asperiores libero consectetur. Praesentium sint itaque, eaque assumenda expedita totam dignissimos molestias sequi natus placeat, aliquid sapiente atque.</p>
-        <h3>Vanmcouver</h3>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto sit ad, aspernatur autem temporibus et ipsam, consequatur excepturi delectus, quo ratione! Quam explicabo repudiandae commodi delectus saepe, adipisci laborum quaerat!</p>
+        <p class="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius enim, minus cumque asperiores libero consectetur. Praesentium sint itaque, eaque assumenda expedita totam dignissimos molestias sequi natus placeat, aliquid sapiente atque.</p>
+        <div class="m-5">
+            <h3 class="mx-5">Vancouver</h3>
+            <p class="text-justify text-uppercase mb-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto sit ad, aspernatur autem temporibus et ipsam, consequatur excepturi delectus, quo ratione! Quam explicabo repudiandae commodi delectus saepe, adipisci laborum quaerat!</p>
+        </div>
         <h4>Comsewogue</h4>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam dolores eos quasi nostrum ducimus nam pariatur molestias consequuntur mollitia recusandae illo accusantium laboriosam debitis minus, quia, cum omnis. Quod, et!</p>
+        <p>psum dolor sit, amet consectetur adipisicing elit. Quibusdam dolores eos quasi nostrum ducimus nam pariatur molestias consequuntur mollitia</p>
+        <p class="blockquote border-left pl-4 font-italic">"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam dolores eos quasi nostrum ducimus nam pariatur molestias consequuntur mollitia recusandae illo accusantium laboriosam debitis minus, quia, cum omnis. Quod, et!"</p>
     </section>
+    </div>
+    </div>
 </div>
 
 <!-- jQuery -->
